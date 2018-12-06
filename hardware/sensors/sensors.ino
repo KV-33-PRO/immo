3badd72ab9837f47fbc1e2859494e02a2ac5dc0e
Roman Lyskov <lyskov8@gmail.com>
2018-12-06 14:36:32 +0300

    Sensors MCU firmware
    Ultrasonic rangefinder now works

--- hardware/sensors/sensors.ino
+++ hardware/sensors/sensors.ino
@@ -1,6 +1,7 @@
 #include <ros.h>
 #include <ros/time.h>
 #include <sensor_msgs/Range.h>
+#include <NewPing.h>  
 
 #define RATE_MS 10
 
@@ -18,11 +19,13 @@ ros::Publisher snr_l_pub( "range/front/left_snr", &snr_l_msg);
 ros::Publisher snr_r_pub( "range/front/right_snr", &snr_r_msg);
 
 unsigned long last_ms;
-
+NewPing sonar_l(4, 3); 
+NewPing sonar_r(6, 5); 
 
 void setup()
 { 
- // nh.getHardware()->setBaud(115200);
+
+  
   nh.initNode();
 
   nh.advertise(ir_l_pub);
@@ -42,13 +45,11 @@ void setup()
   ir_r_msg.min_range = 0.03;
   ir_r_msg.max_range = 0.8;
 
-  //ir_l_msg.radiation_type = sensor_msgs::Range::ULTRASONIC;
   snr_l_msg.header.frame_id =  "/left_snr";
   snr_l_msg.field_of_view = 0.10000000149;
   snr_l_msg.min_range = 0.3;
   snr_l_msg.max_range = 4.0;
 
-  //ir_r_msg.radiation_type = sensor_msgs::Range::ULTRASONIC;
   snr_r_msg.header.frame_id =  "/right_snr";
   snr_r_msg.field_of_view = 0.10000000149;
   snr_r_msg.min_range = 0.3;
@@ -68,11 +69,11 @@ void loop()
     ir_r_msg.header.stamp = nh.now();
     ir_r_pub.publish(&ir_r_msg); 
 
-    snr_r_msg.range = getSNRRange(3,4);
+    snr_r_msg.range = getSNRRange(1);
     snr_r_msg.header.stamp = nh.now();
     snr_r_pub.publish(&snr_r_msg); 
 
-    snr_l_msg.range = getSNRRange(5,6);
+    snr_l_msg.range = getSNRRange(0);
     snr_l_msg.header.stamp = nh.now();
     snr_l_pub.publish(&snr_l_msg); 
  
@@ -91,15 +92,12 @@ float getIRRange(int analogPin) {
   
 }
 
-float getSNRRange(int echoPin, int trigPin) {
-  float duration;
-  float dist;
-  digitalWrite(trigPin, LOW); // Clears the trigPin
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH); // Sets the trigPin on HIGH state for 10 micro seconds
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH);   // Reads the echoPin, returns the sound wave travel time in microseconds
-  dist = duration*0.034/2; // Calculating the distance in cm
-  return dist*100;
+float getSNRRange(int number) {
+float duration;
+float dist;
+  if (number == 0)
+    duration = sonar_l.ping();
+  else duration = sonar_r.ping();
+  dist = duration / US_ROUNDTRIP_CM;
+  return dist/100;
   }
