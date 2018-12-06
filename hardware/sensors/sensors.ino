0a5efc70faddf29ea3c3d02d5be78725a54b4bf9
RomanLyskov <lyskov8@gmail.com>
2018-12-06 13:14:52 +0300

    update sensors MCU firmware

--- hardware/sensors/sensors.ino
+++ hardware/sensors/sensors.ino
@@ -22,11 +22,13 @@ unsigned long last_ms;
 
 void setup()
 { 
-  nh.getHardware()->setBaud(500000);
+ // nh.getHardware()->setBaud(115200);
   nh.initNode();
 
   nh.advertise(ir_l_pub);
-  nh.advertise(ir_r_pub); 
+  nh.advertise(ir_r_pub);
+  nh.advertise(snr_l_pub);
+  nh.advertise(snr_r_pub);  
   
   ir_l_msg.radiation_type = sensor_msgs::Range::INFRARED;
   ir_l_msg.header.frame_id =  "/left_ir";
@@ -41,16 +43,16 @@ void setup()
   ir_r_msg.max_range = 0.8;
 
   //ir_l_msg.radiation_type = sensor_msgs::Range::ULTRASONIC;
-  ir_l_msg.header.frame_id =  "/left_snr";
-  ir_l_msg.field_of_view = 0.10000000149;
-  ir_l_msg.min_range = 0.3;
-  ir_l_msg.max_range = 4.0;
+  snr_l_msg.header.frame_id =  "/left_snr";
+  snr_l_msg.field_of_view = 0.10000000149;
+  snr_l_msg.min_range = 0.3;
+  snr_l_msg.max_range = 4.0;
 
   //ir_r_msg.radiation_type = sensor_msgs::Range::ULTRASONIC;
-  ir_r_msg.header.frame_id =  "/right_snr";
-  ir_r_msg.field_of_view = 0.10000000149;
-  ir_r_msg.min_range = 0.3;
-  ir_r_msg.max_range = 4.0;
+  snr_r_msg.header.frame_id =  "/right_snr";
+  snr_r_msg.field_of_view = 0.10000000149;
+  snr_r_msg.min_range = 0.3;
+  snr_r_msg.max_range = 4.0;
 }
 
 void loop()
