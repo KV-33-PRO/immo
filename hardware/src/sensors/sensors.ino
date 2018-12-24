49969fae3d24d42b82484220619ae375e7aa98a4
RossolovRoman <rrossolov@bk.ru>
2018-12-24 15:17:12 +0300

    Fix firmware sensors (stm32). Add library Ultrasonic. Delete library NewPing.

--- hardware/src/sensors/sensors.ino
+++ hardware/src/sensors/sensors.ino
@@ -1,65 +1,63 @@
 #include <ros.h>
-#include <ros/time.h>
 #include <sensor_msgs/Range.h>
-#include <NewPing.h>  
+#include <Ultrasonic.h>
 
-#define RATE_MS 10
+#define RATE_MS       20
 
 ros::NodeHandle nh;
 
 sensor_msgs::Range snr_l_msg;
 sensor_msgs::Range snr_r_msg;
 
-ros::Publisher snr_l_pub( "range/front/left_snr", &snr_l_msg);
-ros::Publisher snr_r_pub( "range/front/right_snr", &snr_r_msg);
+ros::Publisher snr_l_pub("range/front/left_snr", &snr_l_msg);
+ros::Publisher snr_r_pub("range/front/right_snr", &snr_r_msg);
 
 unsigned long last_ms;
-NewPing sonar_l(4, 3); 
-NewPing sonar_r(6, 5); 
+Ultrasonic sonar_l(PA15, PB3);
+Ultrasonic sonar_r(PB4, PB5);
+
 
 void setup()
-{ 
-
+{
   nh.getHardware()->setBaud(500000);
   nh.initNode();
   nh.advertise(snr_l_pub);
-  nh.advertise(snr_r_pub);  
+  nh.advertise(snr_r_pub);
 
   snr_l_msg.header.frame_id =  "/left_snr";
   snr_l_msg.field_of_view = 0.10000000149;
   snr_l_msg.min_range = 0.3;
-  snr_l_msg.max_range = 4.0;
+  snr_l_msg.max_range = 3.5;
 
   snr_r_msg.header.frame_id =  "/right_snr";
   snr_r_msg.field_of_view = 0.10000000149;
   snr_r_msg.min_range = 0.3;
-  snr_r_msg.max_range = 4.0;
+  snr_r_msg.max_range = 3.5;
 }
 
 void loop()
 {
-  if((millis() - last_ms) > RATE_MS){
+  if ((millis() - last_ms) > RATE_MS) {
     last_ms = millis();
-
-    snr_r_msg.range = getSNRRange(1);
-    snr_r_msg.header.stamp = nh.now();
-    snr_r_pub.publish(&snr_r_msg); 
 
     snr_l_msg.range = getSNRRange(0);
     snr_l_msg.header.stamp = nh.now();
-    snr_l_pub.publish(&snr_l_msg); 
- 
-  }
+    snr_l_pub.publish(&snr_l_msg);
 
+    snr_r_msg.range = getSNRRange(1);
+    snr_r_msg.header.stamp = nh.now();
+    snr_r_pub.publish(&snr_r_msg);
+
+  }
   nh.spinOnce();
 }
 
-float getSNRRange(int number) {
-float duration;
-float dist;
-  if (number == 0)
-    duration = sonar_l.ping();
-  else duration = sonar_r.ping();
-  dist = duration / US_ROUNDTRIP_CM;
-  return dist/100;
+float getSNRRange(int side) {
+  if (side == 0) {
+    return sonar_l.read()/100.0;
   }
+  else
+  {
+    return sonar_r.read()/100.0;
+  }
+}
